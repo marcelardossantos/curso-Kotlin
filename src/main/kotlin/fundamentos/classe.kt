93bcbb7a5c2d36335d84d7c25ba63f1b6efae366
Marcela R. dos Santos <marcelarsantossi@hotmail.com>
2023-06-05 18:33:28 -0300

    Estuda Classes, Objetos e Construtores

--- src/main/kotlin/fundamentos/classe.kt
+++ src/main/kotlin/fundamentos/classe.kt
@@ -1,4 +1,12 @@
 package fundamentos
 
-class classe {
+class Pessoa(var nome: String, var idade: Int) {
+    override fun toString(): String {
+        return "Classe: Pessoa. Nome: ${nome}, Idade${idade}"
+    }
+}
+
+fun main() {
+    var marcela = Pessoa ("Marcela", 39)
+    println(marcela)
 }
