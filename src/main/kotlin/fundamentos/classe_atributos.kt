05f4826f1af29ae12ff497e8d77b9046d8970521
Marcela R. dos Santos <marcelarsantossi@hotmail.com>
2023-06-05 22:42:06 -0300

    Estuda Atributos e acessos

--- src/main/kotlin/fundamentos/classe_atributos.kt
+++ src/main/kotlin/fundamentos/classe_atributos.kt
@@ -1,2 +1,22 @@
 package fundamentos
 
+class Carro( var cor:String, val anoFabricacao: Int, var dono: Dono) {
+
+}
+
+data class Dono(var nome: String, var idade: Int) {
+
+}
+fun main() {
+    var carro = Carro("Branco", 2023, Dono("Marcela", 39))
+
+    println(carro.cor)
+    carro.cor = "Preto"
+
+    println(carro.cor)
+
+    println(carro.dono.nome)
+    carro.dono.nome = "Yasmin"
+
+    println(carro.dono)
+}
