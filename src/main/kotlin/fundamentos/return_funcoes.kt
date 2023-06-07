1d1c596f9bcb30a2935a9d57fbbd2cc128b715f4
Marcela R. dos Santos <marcelarsantossi@hotmail.com>
2023-06-06 23:30:14 -0300

    Estuda Multiplos retornos da função

--- src/main/kotlin/fundamentos/return_funcoes.kt
+++ src/main/kotlin/fundamentos/return_funcoes.kt
@@ -1,4 +1,17 @@
 package fundamentos
 
-class return_funcoes {
+fun retornaNumeroPorExtenso(numero: Int): String {
+    if (numero == 4) {
+        return "Quatro"
+    } else if (numero == 8) {
+        return "Oito"
+    } else {
+        return "Número não mapeado"
+    }
+}
+
+fun main() {
+    println(retornaNumeroPorExtenso(4))
+    println(retornaNumeroPorExtenso(8))
+    println(retornaNumeroPorExtenso(9))
 }
