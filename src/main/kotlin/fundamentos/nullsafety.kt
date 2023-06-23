7653f784487a68b7a08934a5da4501b8e61a54a3
Marcela R. dos Santos <marcelarsantossi@hotmail.com>
2023-06-23 16:48:38 -0300

    Estuda Null Safety

--- src/main/kotlin/fundamentos/nullsafety.kt
+++ src/main/kotlin/fundamentos/nullsafety.kt
@@ -1,2 +1,26 @@
 package fundamentos
 
+import java.lang.reflect.UndeclaredThrowableException
+
+fun main(){
+
+    var lista : List<Int?> = listOf(1, 2, null, 4)
+    println(lista)
+
+    var listNullable : List<Int?>? = null // Neste caso a lista pode ser nula e conter elementos nulos//
+    println(listNullable)
+
+//    var nome : String? = null
+//
+//    var tamanho = nome?.length ?: 0
+//
+//    println(tamanho)
+
+//    var nome : String? = "Marcela"
+//
+//    if (nome != null) {
+//        println(nome.length)
+//    }
+//
+//    val toShort : Short = nome!!.length.toShort()
+}
