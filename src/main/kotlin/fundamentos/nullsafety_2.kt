4f70cfc567cf8000daa17b3b980db98369f4b874
Marcela R. dos Santos <marcelarsantossi@hotmail.com>
2023-06-23 17:05:17 -0300

    Estuda Null Safety - Parte 2

--- src/main/kotlin/fundamentos/nullsafety_2.kt
+++ src/main/kotlin/fundamentos/nullsafety_2.kt
@@ -1,2 +1,12 @@
 package fundamentos
 
+fun main() {
+//    val pessoa: Pessoa? = Pessoa("Marcela", 39)
+//
+//    println(pessoa!!.nome)
+
+    val pessoa: Pessoa? = null
+
+    println(pessoa!!.nome)
+    println("fim do código")
+}
