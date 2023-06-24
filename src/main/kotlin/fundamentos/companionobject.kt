bf85106387dbf82ef5c5bc223982291f843310b9
Marcela R. dos Santos <marcelarsantossi@hotmail.com>
2023-06-23 22:08:08 -0300

    Estuda Companion Object

--- src/main/kotlin/fundamentos/companionobject.kt
+++ src/main/kotlin/fundamentos/companionobject.kt
@@ -1,2 +1,35 @@
 package fundamentos
 
+class MinhaClasse (
+    var nome: String,
+    var endereco: String,
+    var idade: Int
+) {
+    companion object {
+        fun criarComValoresPadrão(): MinhaClasse {
+            return MinhaClasse("Marcela",  "Rua dos Bobos", 39)
+        }
+
+        fun criarAPartirDeSegundaClasse(segundaClasse: SegundaClasse): MinhaClasse {
+            return MinhaClasse(segundaClasse.nome, segundaClasse.endereco, segundaClasse.idade) //Builder
+        }
+    }
+}
+
+class SegundaClasse(
+    var nome: String,
+    var endereco: String,
+    var idade: Int
+) {
+    fun criaComValoresPadrao(): SegundaClasse {
+        return SegundaClasse("Marcela", "Rua dos Bobos", 39)
+
+    }
+}
+
+fun main() {
+    var segundaClasse = SegundaClasse("nome", "endereco", 10).criaComValoresPadrao()
+
+    var minhacLoader = MinhaClasse.criarAPartirDeSegundaClasse(segundaClasse)
+
+}
